33f589a52a427b6046e1fc8da384cb4fbabc62a1
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2018-12-02 17:45:57 +0100

    test: add more valid options

--- tests/lib/rules/jsx-spread.js
+++ tests/lib/rules/jsx-spread.js
@@ -1,16 +1,14 @@
 /**
  * @fileoverview spread over jsx
- * @author verdaccio
+ * @author Juan Picado <juanpicado19@gmail.com>
  */
 'use strict';
 
 // ------------------------------------------------------------------------------
 // Requirements
 // ------------------------------------------------------------------------------
-const rule = require('../../../lib/rules/jsx-spread'),
-
-  RuleTester = require('eslint').RuleTester;
-
+const rule = require('../../../lib/rules/jsx-spread');
+const RuleTester = require('eslint').RuleTester;
 
 // ------------------------------------------------------------------------------
 // Tests
@@ -30,23 +28,46 @@ ruleTester.run('jsx-spread', rule, {
   valid: [
     '<div foo="1" bar="1"/>',
     '<div/>',
-    '<div foo bar/>'
+    '<div foo bar/>',
+    {
+      code: '<div {...props}/>',
+      options: ['never'],
+      parser: 'babel-eslint'
+    },
+    {
+      code: '<div {...props} foo={"1"}/>',
+      options: ['never']
+    }
   ],
 
   invalid: [
     {
       code: '<div {...props}/>',
       errors: [{
-        message: 'spread operator are not allowed on jsx components.',
+        message: rule.ERROR_MESSAGE,
         type: 'JSXOpeningElement'
       }]
     },
     {
-        code: '<div foo {...props}/>',
-        errors: [{
-          message: 'spread operator are not allowed on jsx components.',
-          type: 'JSXOpeningElement'
-        }]
+      code: '<div foo {...props}/>',
+      errors: [{
+        message: rule.ERROR_MESSAGE,
+        type: 'JSXOpeningElement'
+      }]
+    },
+    {
+      code: '<div foo="1" {...props}/>',
+      errors: [{
+        message: rule.ERROR_MESSAGE,
+        type: 'JSXOpeningElement'
+      }]
+    },
+    {
+      code: '<Component foo="1" {...props}>test</Component>',
+      errors: [{
+        message: rule.ERROR_MESSAGE,
+        type: 'JSXOpeningElement'
+      }]
     }
   ]
 });
